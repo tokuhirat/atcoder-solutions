a62d9c5121143f88c003e777b13dde363505c8bf
toku <sgsnknch@gmail.com>
2024-11-23 09:43:43 +0900

    abc381d again

--- abc/abc381/d.rs
+++ abc/abc381/d.rs
@@ -12,47 +12,29 @@ fn main() {
     for i in 0..n - 1 {
         same.push(a[i] == a[i + 1]);
     }
-    let mut right = 0;
     let mut ans = 0;
-    let mut seen = HashSet::new();
 
-    let mut left = 0;
-    while left < n - 1 {
-        if !same[left] {
+    for start in [0, 1] {
+        let mut left = start;
+        let mut right = 0;
+        let mut seen = HashSet::new();
+        while left < n - 1 {
+            if !same[left] {
+                left += 2;
+                continue;
+            }
+            seen.insert(a[left]);
+
+            right = right.max(left + 2);
+            while right < n - 1 && same[right] && !seen.contains(&a[right]) {
+                seen.insert(a[right]);
+                right += 2;
+            }
+            ans = ans.max(right - left);
+            seen.remove(&a[left]);
             left += 2;
-            continue;
         }
-        seen.insert(a[left]);
-
-        right = right.max(left + 2);
-        while right < n - 1 && same[right] && !seen.contains(&a[right]) {
-            seen.insert(a[right]);
-            right += 2;
-        }
-        ans = ans.max(right - left);
-        seen.remove(&a[left]);
-        left += 2;
     }
 
-    left = 1;
-    right = 0;
-    let mut seen = HashSet::new();
-    while left < n - 1 {
-        if !same[left] {
-            left += 2;
-            continue;
-        }
-        seen.insert(a[left]);
-
-        right = right.max(left + 2);
-        while right < n - 1 && same[right] && !seen.contains(&a[right]) {
-            seen.insert(a[right]);
-
-            right += 2;
-        }
-        ans = ans.max(right - left);
-        seen.remove(&a[left]);
-        left += 2;
-    }
     println!("{}", ans);
 }
