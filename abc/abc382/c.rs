306d2ce2df0de0da5a5eaaf2f59f03763099ee7a
toku <sgsnknch@gmail.com>
2024-12-01 00:55:01 +0900

    abc382c

--- abc/abc382/c.rs
+++ abc/abc382/c.rs
@@ -4,34 +4,20 @@ use proconio::{fastout, input};
 fn main() {
     input!(
         (n, m): (usize, usize),
-        a: [usize; n],
+        mut a: [usize; n],
         b: [usize; m],
     );
 
-    let mut aa: Vec<(usize, usize)> = Vec::with_capacity(n);
-    for (i, &ai) in a.iter().enumerate() {
-        if i == 0 || ai < aa.last().unwrap().0 {
-            aa.push((ai, i + 1));
-        }
+    for i in 1..n {
+        a[i] = a[i - 1].min(a[i])
     }
-    let mut ans = Vec::with_capacity(m);
-    aa.reverse();
+
     for bi in &b {
-        let i = aa.binary_search_by(|&e| e.0.cmp(bi));
-        match i {
-            Ok(i) => {
-                ans.push(aa[i].1 as i64);
-            }
-            Err(i) => {
-                if i == 0 {
-                    ans.push(-1);
-                } else {
-                    ans.push(aa[i - 1].1 as i64);
-                }
-            }
+        let i = a.partition_point(|e| e > bi);
+        if i == n {
+            println!("-1");
+        } else {
+            println!("{}", i + 1);
         }
-    }
-    for ans_i in &ans {
-        println!("{}", ans_i);
     }
 }
