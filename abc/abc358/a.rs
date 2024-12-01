b5a913945b08b3cc726285e3c8792c68599867ea
toku <sgsnknch@gmail.com>
2024-12-01 17:53:13 +0900

    abc358a

---
+++ abc/abc358/a.rs
@@ -0,0 +1,14 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input!(
+        s: String,
+        t: String,
+    );
+    if s == "AtCoder" && t == "Land" {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
