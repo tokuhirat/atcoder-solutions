4606d7cf338bf0a2550bbf3de3478f906c6212eb
toku <sgsnknch@gmail.com>
2025-01-01 18:57:57 +0900

    dp_c

--- other/dp/c.rs
+++ other/dp/c.rs
@@ -8,17 +8,14 @@ fn main() {
         n: usize,
     };
     let mut dp = [0, 0, 0];
-    for i in 0..n {
+    for _ in 0..n {
         input! {(a, b, c): (usize, usize, usize)};
-        if i == 0 {
-            dp = [a, b, c];
-        } else {
-            let mut old = [0, 0, 0];
-            swap(&mut dp, &mut old);
-            dp[0] = old[1].max(old[2]) + a;
-            dp[1] = old[2].max(old[0]) + b;
-            dp[2] = old[0].max(old[1]) + c;
-        }
+
+        let mut old = [0, 0, 0];
+        swap(&mut dp, &mut old);
+        dp[0] = old[1].max(old[2]) + a;
+        dp[1] = old[2].max(old[0]) + b;
+        dp[2] = old[0].max(old[1]) + c;
     }
     println!("{}", dp.iter().max().unwrap());
 }
